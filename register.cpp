1c86362a4cd9f159ea37c86042f02ed3cfb64204
WwangSeven <2567796636@qq.com>
2018-12-04 21:13:11 +0800

    change two

--- register.cpp
+++ register.cpp
@@ -44,69 +44,76 @@ void Register::on_Sure_Button_clicked()
     QString info;
     // to do: 完善表单验证
     //用户名校验
+    int flag=0;
+    for(int i=0;i<RealName.length();i++)
+    {
+        if((RealName.at(i)>=1 &&RealName.at(i)<=64)||(RealName.at(i)>=91&&RealName.at(i)<=96)||(RealName.at(i)>=123&&RealName.at(i)<=127))
+        {
+            flag=1;
+        }
+    }
 
     if(UserName.length()==0)
     {
         info="用户名不能为空";
         SentMessage(info);
+        qDebug() << "1" << endl;
     }
-
-//    else
-//    {
-//            QByteArray ba=UserName.toLatin1();//QString 转换为 char*
-//            const char *s=ba.data();
-//              int len=0;
-//            for(*s;*s;s++)
-//            {
-//                if(*s<'0'&&*s>'9'&&*s!='_'&&*s<'a'&&*s>'z'&&*s>'Z'&&*s<'A')
-//                {
-//                    info="用户名仅支持中英文、数字和下划线";
-//                    SentMessage(info);
-//                }
-//            }
-//            while(*s&&*s>='0'&&*s<='9') {len++;s++;}
-//            if(len==UserName.length)//是纯数字
-//            {
-//                info="用户名不能为纯数字";
-//                SentMessage(info);
-//            }
-//    }
+    else if(UserName.length()>=15)
+    {
+        info="用户名不能超过15个字符";
+        SentMessage(info);
+        qDebug() << "2" << endl;
+    }
     else if(PassWord.length()==0)
     {
         info="密码不能为空";
         SentMessage(info);
+        qDebug() << "3" << endl;
     }
     else if(PassWord!=PassWordTwo)
     {
         info="您输入的两次密码不同";
+        SentMessage(info);
+        qDebug() << "4" << endl;
+    }
+    else if(RealName.length()==0)
+    {
+        info="真实姓名不能为空";
+        SentMessage(info);
+        qDebug() << "5" << endl;
+    }
+    else if(flag)
+    {
+        info="真实姓名不合法";
         SentMessage(info);
     }
     else if(PhoneNumber.length()==0)
     {
         info="手机号码不能为空";
         SentMessage(info);
+        qDebug() << "7" << endl;
     }
     else if(PhoneNumber.length()!=11)
     {
         info="您的手机号输入有误,请重新输入";
         SentMessage(info);
+        qDebug() << "8" << endl;
     }
-    else if(PhoneNumber.mid(0,2)!="13")
+    else if(PhoneNumber.mid(0,2)!="13"&&PhoneNumber.mid(0,2)!="18"&&(PhoneNumber.mid(0,2)!="15"||PhoneNumber.mid(0,3)=="154")&&PhoneNumber.mid(0,3)!="145"&&PhoneNumber.mid(0,3)!="147")
     {
-        if(PhoneNumber.mid(0,2)!="18")
-        {
-            if(PhoneNumber.mid(0,2)!="15"||PhoneNumber.mid(0,3)=="154")
-            {
-                if(PhoneNumber.mid(0,3)!="145"&&PhoneNumber.mid(0,3)!="147")
-                {
-                    info="您的手机号不合法,请重新输入";
-                    SentMessage(info);
-                }
-            }
-        }
+        info="您的手机号不合法,请重新输入";
+        SentMessage(info);
+        qDebug() << "9" << endl;
+    }
+    else if(IdCard.length()!=18)
+    {
+        info="身份证号码不合法";
+        SentMessage(info);
     }
     else
     {
+        qDebug() << "10" << endl;
         // to do: write the data to csv
 
         // issue: 这里使用相对路径原因是工作路径和exe文件构建路径不在一个地方
