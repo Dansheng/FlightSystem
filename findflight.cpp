2248b1565d2bfe2a76d8703d3d9abaf65f71176e
Dansheng <34390574+Dansheng@users.noreply.github.com>
2018-12-07 11:23:50 +0800

    solve the problem that read data

--- findflight.cpp
+++ findflight.cpp
@@ -20,6 +20,7 @@ typedef struct AirInfoNode{
 typedef struct FlightList{
     Flight Front;//指向航线头指针
     Flight Rear;//指向航线尾指针
+    int length;
 }FlightList;
 
 const QString FILE_PATH("..\\assets\\flight\\flight.csv");//File_PATH为文件保存的路径及类型
@@ -32,21 +33,21 @@ FindFlight::FindFlight(QWidget *parent) :
 {
     ui->setupUi(this);
     // CityStart
-    ui->CityStart_comboBox->addItem(tr("北京"));
-    ui->CityStart_comboBox->addItem(tr("西安"));
-    ui->CityStart_comboBox->addItem(tr("厦门"));
-    ui->CityStart_comboBox->addItem(tr("上海浦东"));
-    ui->CityStart_comboBox->addItem(tr("上海虹桥"));
-    ui->CityStart_comboBox->addItem(tr("深圳"));
-    ui->CityStart_comboBox->addItem(tr("广东"));
+    ui->CityStart_ComboBox->addItem(tr("北京"));
+    ui->CityStart_ComboBox->addItem(tr("西安"));
+    ui->CityStart_ComboBox->addItem(tr("厦门"));
+    ui->CityStart_ComboBox->addItem(tr("上海浦东"));
+    ui->CityStart_ComboBox->addItem(tr("上海虹桥"));
+    ui->CityStart_ComboBox->addItem(tr("深圳"));
+    ui->CityStart_ComboBox->addItem(tr("广东"));
     // EndStart
-    ui->CityEnd_comboBox->addItem(tr("北京"));
-    ui->CityEnd_comboBox->addItem(tr("西安"));
-    ui->CityEnd_comboBox->addItem(tr("厦门"));
-    ui->CityEnd_comboBox->addItem(tr("上海浦东"));
-    ui->CityEnd_comboBox->addItem(tr("上海虹桥"));
-    ui->CityEnd_comboBox->addItem(tr("深圳"));
-    ui->CityEnd_comboBox->addItem(tr("广东"));
+    ui->CityStart_ComboBox->addItem(tr("北京"));
+    ui->CityStart_ComboBox->addItem(tr("西安"));
+    ui->CityStart_ComboBox->addItem(tr("厦门"));
+    ui->CityStart_ComboBox->addItem(tr("上海浦东"));
+    ui->CityStart_ComboBox->addItem(tr("上海虹桥"));
+    ui->CityStart_ComboBox->addItem(tr("深圳"));
+    ui->CityStart_ComboBox->addItem(tr("广东"));
     ReadData();
 }
 //读取csv文件中的信息，将其串成一个单链表
@@ -57,70 +58,52 @@ void ReadData()
     QStringList CSVList,list;  //字符串列表
     QString Str;
     CSVList.clear();//清空
-    Flight S,P;
-//    FlightList L;
-
+    Flight S;
+    // 初始化L,并以L的Front为头结点
+    FlightList L;
+    L.length=0;
+    L.Front=new AirInfoNode; // 头结点
+    L.Front->next=NULL; // 置L的头结点为空
+    L.Rear->next=NULL;
     if(csvFile.open(QIODevice::ReadWrite))
     {
         QTextCodec *codec = QTextCodec::codecForName("UTF-8");
-        Str = codec->toUnicode(csvFile.readLine());
-        list = Str.split(",");
-        S= new AirInfoNode;
-        S->StartPla=list[0];
-        S->EndPla=list[1];
-        S->FlightNum=list[2];
-        S->PlaneNum=list[3];
-        S->TimeFly=list[4];
-        S->TimeArr=list[5];
-        S->date=list[6];
-        S->Price=list[7].toInt();
-        S->Discount=list[8];
-        S->PassNum=list[8].toInt();
-        S->TicketsRest=list[9].toInt();
 
+        // 单链表
+        int count=18; // 目前已知数据有18条，由于csv文件的行数读取 利用at.end()方法并不奏效
+                      // 因此暂时设定为常数
+                      // 解决该问题的思路是，设立另外一个保存csv文件行数的count.txt 每次插入数据更新 count.txt
+        for(int i=0;i<count;i++)
+        {
+            Str = codec->toUnicode(csvFile.readLine());
+            list = Str.split(",");
+            S= new AirInfoNode;
+            // 储存数据
+            S->StartPla=list[0];
+            S->EndPla=list[1];
+            S->FlightNum=list[2];
+            S->PlaneNum=list[3];
+            S->TimeFly=list[4];
+            S->TimeArr=list[5];
+            S->date=list[6];
+            S->Price=list[7].toInt();
+            S->Discount=list[8];
+            S->PassNum=list[8].toInt();
+            S->TicketsRest=list[9].toInt();
+            // 头插法
+            S->next=L.Front->next;
+            L.Front->next=S;
+            L.length++;
+        }
+        qDebug() << "成功读入行数:" << L.length << endl;
+        // 事实上，L.Front->next指向的是最后一个插入的元素
+        // L.Rear 指向的是第一个插入的元素
+        // 因此我考虑是否调换Front 和 Rear的名称
     }
     csvFile.close();
-
     qDebug() << S->StartPla << endl;
     qDebug() << S->TicketsRest << endl;
 
-//    Str= codec->toUnicode(stream.readLine());
-//    qDebug() << Str << endl;
-//    list=Str.split(",");
-//    qDebug() << list.at(0) << endl;
-//    qDebug() << list.at(1) << endl;
-//    qDebug() << list.at(2) << endl;
-//    qDebug() << list.at(3) << endl;
-
-//    L.Front->next=NULL;
-//    P=L.Front;
-//    int i=0;
-//    if (csvFile.open(QIODevice::ReadWrite))  //打开成功
-//       {
-//           QTextStream stream(&csvFile); //读取文件csvFile
-//            while (!stream.atEnd())
-//               {
-//                   Str=stream.readLine(i);//读取txt文件中每一行的航班信息，作为单链表的一个结点
-//                   list =Str.split(",");
-//                   S=new AirInfoNode;
-//                   S->StartPla=list[0];
-//                   S->EndPla=list[1];
-//                   S->FlightNum=list[2];
-//                   S->PlaneNum=list[3];
-//                   S->TimeFly=list[4];
-//                   S->TimeArr=list[5];
-//                   S->Price=list[6];
-//                   S->Discount=list[7];
-//                   S->PassNum=list[8].toInt();//乘员定额字符串转数字
-//                   S->TicketsRest=list[9].toInt();//余票量字符串转数字
-//                   S->next=NULL;
-//                   P->next=S;
-//                   P=P->next;
-//                   L.Rear=S;
-//                   i++;
-//                }
-//             csvFile.close();
-//            }
 }
 
 FindFlight::~FindFlight()
--- findflight.h
+++ findflight.h
@@ -16,7 +16,6 @@ public:
     ~FindFlight();
 
 private slots:
-    void on_pushButton_clicked();
 
 private:
     Ui::FindFlight *ui;
